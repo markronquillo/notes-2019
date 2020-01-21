0462f4a307f2732ab6dc882ef107ccfb46a027e5
Mark Joseph Ronquillo <markronquillo23@gmail.com>
2020-01-21 20:56:40 +0800

    Mutation sectioN

---
+++ GraphQL/apollo-docs-tutorial/fullstack-tutorial/start/server/src/schema.js
@@ -0,0 +1,65 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  # Your schema will go here
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(mission: String, size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Query {
+    launches(pageSize: Int!, after: String): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`;
+
+module.exports = typeDefs;
